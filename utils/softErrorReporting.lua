a0eaa49c6dd3c69d33ab709d5a288a9558617221
Daniel Brauer <danielbrauer@unity3d.com>
2019-04-22 15:11:32 +0200

    Use proper API for multiplayer check

--- utils/softErrorReporting.lua
+++ utils/softErrorReporting.lua
@@ -3,14 +3,8 @@ local softErrorReporting = {}
 
 local haveShownError = false
 
-function softErrorReporting.getCount(table)
-    local count = 0
-    for _ in pairs(table) do count = count + 1 end
-    return count
-end
-
 function softErrorReporting.showModError(message)
-    if not haveShownError and softErrorReporting.getCount(game.players) == 1 then
+    if not haveShownError and not game.is_multiplayer() then
         game.show_message_dialog{text = {"", {message}, {"errors.please-report"}}}
         haveShownError = true
     end
