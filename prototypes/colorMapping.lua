fb2bfb8d78fc25488f74ef4c1e552daaac53757b
Daniel Brauer <danielbrauer@unity3d.com>
2019-04-28 14:33:33 +0200

    Handle case where an ingredient has a missing icon

--- prototypes/colorMapping.lua
+++ prototypes/colorMapping.lua
@@ -27,21 +27,36 @@ local iconColors =
 }
 local ingredientColors = {}
 local missing = {}
+local foundMissing = false
+local missingIcons = {}
+local foundMissingIcons = false
 
 for _, tech in pairs(data.raw["technology"]) do
     for _, ingredientPair in ipairs(tech.unit.ingredients) do
         local ingredientName = ingredientPair[1]
         local ingredient = data.raw.tool[ingredientName]
-        if not ingredientColors[ingredientName] then
-            ingredientColors[ingredientName] = iconColors[ingredient.icon]
-            if not ingredientColors[ingredientName] and not missing[ingredient.icon] then
-                missing[ingredient.icon] = true
+        if not ingredientColors[ingredientName] and not missingIcons[ingredientName] then
+            if ingredient.icon then
+                ingredientColors[ingredientName] = iconColors[ingredient.icon]
+                if not ingredientColors[ingredientName] and not missing[ingredient.icon] then
+                    missing[ingredient.icon] = true
+                    foundMissing = true
+                end
+            elseif not missingIcons[ingredientName] then
+                missingIcons[ingredientName] = true
+                foundMissingIcons = true
             end
         end
     end
 end
 
-log("Missing colours for the following icons: "..serpent.block(missing))
+if foundMissing then
+    log("Missing colours for the following icons: "..serpent.block(missing))
+end
+
+if foundMissingIcons then
+    log("The following ingredients have no icons: "..serpent.block(missingIcons))
+end
 
 local index = 1
 for name, color in pairs(ingredientColors) do
