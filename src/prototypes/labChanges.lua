b71f6d18b36ceb5f09818005fb95aa5b830b5760
PennyJim <git@pennyjim.com>
2026-03-09 16:37:29 -0600

    Remove deprecated use of hr_version

--- src/prototypes/labChanges.lua
+++ src/prototypes/labChanges.lua
@@ -20,28 +20,16 @@ labChanges.labStorm =
 {
     type = "animation",
     name = "discoscience-lab-storm",
-    filename = "__DiscoScience__/graphics/lab-storm.png",
+    filename = "__DiscoScience__/graphics/hr-lab-storm.png",
     blend_mode = "additive",
     draw_as_glow = true,
-    width = 106,
-    height = 100,
+    width = 216,
+    height = 194,
     frame_count = 33,
     line_length = 11,
     animation_speed = 1 / 3,
-    shift = util.by_pixel(-1, 1),
-    hr_version =
-    {
-        filename = "__DiscoScience__/graphics/hr-lab-storm.png",
-        blend_mode = "additive",
-        draw_as_glow = true,
-        width = 216,
-        height = 194,
-        frame_count = 33,
-        line_length = 11,
-        animation_speed = 1 / 3,
-        shift = util.by_pixel(0, 0),
-        scale = 0.5
-    }
+    shift = util.by_pixel(0, 0),
+    scale = 0.5
 }
 
 return labChanges
